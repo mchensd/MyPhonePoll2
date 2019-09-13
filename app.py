54f0c5548862253fb792067b71f46c66a6f52ee8
Michael  Chen <chen.young@Mihos-iPad.wireless.yale.internal>
2019-09-13 15:14:02 -0400

    Initial Commit

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-
+#print(os.environ.get('SECRET_KEY'))
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 print(app.config['SECRET_KEY'])
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
